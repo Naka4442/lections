3af8cdd7249c291c7ccfa033e49ec46b5935ad5c
Naka4442 <nak4442@gmail.com>
2022-02-08 03:02:17 +0300

    Пример на Vue

---
+++ vue/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <main>
+    <button @click="clicks++">{{clicks}}</button>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Vue',
+  data(){
+    return {
+      clicks : 0
+    }
+  },
+  components: {
+  }
+}
+</script>
+<style scoped>
+main{
+  width: 100%;
+  height: 100vh;
+  background-color: #333333;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+button{
+    width: 100px;
+    height: 100px;
+    border-radius: 10px;
+    border: none;
+    border-bottom: 5px solid #1467ac;
+    outline: none;
+    background-color: #50b0ff;
+    font-size: 50px;
+    font-weight: 600;
+    cursor: pointer;
+}
+button:active{
+    box-shadow: 0 0 10px black inset;
+    border-bottom: none;
+}
+</style>
